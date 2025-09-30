eea1ad16a0155d5e6c3c5090d60805a081f89a8f
C0C24005 <c0c24005a8@edu.teu.ac.jp>
2025-09-30 14:14:18 +0900

    練習２：爆弾作った

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,14 +1,15 @@
 import os
+import random
 import sys
 import pygame as pg
 
 
-WIDTH, HEIGHT = 1100, 65
+WIDTH, HEIGHT = 1100, 650
 DELTA = {
     pg.K_UP: (0, -5),
-    pg.K_DOWN: (0, -5),
-    pg.K_LEFT: (0, -5),
-    pg.K_RIGHT: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_LEFT: (-5, 0),
+    pg.K_RIGHT: (+5, 0),
 }
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,6 +21,13 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img = pg.Surface((20, 20))  # 爆弾用の空Surface
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)  # 赤い爆弾円
+    bb_img.set_colorkey((0, 0, 0))  # 四隅の黒い部分を透過
+    bb_rct = bb_img.get_rect()  # 爆弾Rect
+    bb_rct.centerx = random.randint(0, WIDTH)  # 爆弾横座標
+    bb_rct.centery = random.randint(0, HEIGHT)  # 爆弾縦座標
+    vx, vy = +5, +5  # 爆弾の速度
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -32,9 +40,10 @@ def main():
         sum_mv = [0, 0]
         for key, mv in DELTA.items():
             if key_lst[key]:
-                sum_mv[0] += mv[0]
-                sum_mv[1] += mv[1]
-
+                sum_mv[0] += mv[0]  # 横方向の移動量を加算
+                sum_mv[1] += mv[1]  # 縦方向の移動量を加算
+        # if key_lst[pg.K_w]:
+        #     sum_mv[1] -= 5
         # if key_lst[pg.K_UP]:
         #     sum_mv[1] -= 5
         # if key_lst[pg.K_DOWN]:
@@ -45,6 +54,8 @@ def main():
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy)  # 爆弾移動
+        screen.blit(bb_img, bb_rct)  # 爆弾描画
         pg.display.update()
         tmr += 1
         clock.tick(50)
